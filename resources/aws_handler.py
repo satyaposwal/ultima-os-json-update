b89bd814d13c59c4183f6d6a86db8e9139de7656
Shreyas Subhedar <shreyassubhedar@gmail.com>
2022-06-22 10:47:44 +0530

    DEVOPS-1 | fix it

--- resources/aws_handler.py
+++ resources/aws_handler.py
@@ -7,7 +7,7 @@ class AWSHandler():
 
     def s3Download(self, remote_folder_name, local_path) -> None:
         subprocess.run(["aws", "s3", "cp", remote_folder_name,
-                       local_path, "--recursive"])
+                       local_path])
     
     def s3Upload(self,remote_folder_name, local_path):
-        subprocess.run(["aws", "s3", "cp", local_path, remote_folder_name, "--recursive"])
+        subprocess.run(["aws", "s3", "cp", local_path, remote_folder_name])
